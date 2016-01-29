b00a4299de4c2bb88a8d9e0444673b07cf8f7874
utsavsolanki <utsav.solanki@live.com>
2016-01-29 14:18:49 +0530

    abc

--- verctors.R
+++ verctors.R
@@ -1,7 +1,6 @@
-#Code by Utsav Solanki
 fruits <- c("Apple","Bannana","Grapes")
 products <- c("Book1","Book2","Book3")
-Mobile <- c("Samsung","LG","Micromax")
+Mobile <- c("Samsung","LG")
 fruits 
 products
-Mobile 
+
